803da2c8824157e103fddfda773328239dfee93b
johnjsyoo <johnjsyoo@gmail.com>
2014-12-12 22:11:15 -0500

    Final Upload of All Data

--- EnergyData.R
+++ EnergyData.R
@@ -2,19 +2,22 @@ library(ggplot2)
 library(reshape2)
 library(RMySQL)
 
-con=dbConnect(MySQL(),user='root',password='F00disg00d',host='localhost',dbname='IndividualProject')
+# Import all of our data!
+connect = dbConnect(MySQL(), user='root', password='LAX', host='localhost', dbname='wbproject')
 
-combined=dbReadTable(conn=con,name='combined')
-attach(combined)
+energy_data = dbReadTable(conn=connect, name='energydata')
+energy_data = attach(energy_data)
 
+usenergy = dbReadTable(conn=connect, name='usenergy')
+usenergy = attach(usenergy)
 
-setwd("~/Dropbox/IndividualAssignment/")
-energy_data = read.csv("energy_data.csv")
+chnenergy = dbReadTable(conn=connect, name='chnenergy')
+chnenergy = attach(chnenergy)
 
 # Set columns names to something more appropriate and 
 # add the renewable percentage and nonrenewable amounts
 
-colnames(energy_data) = c('country', 'gdp', 'renewable.kwh', 'total.kwh')
+colnames(energy_data) = c('country', 'GDP', 'renewable.kwh', 'total.kwh')
 energy_data$renewable.percent = energy_data$renewable.kwh/energy_data$total.kwh
 energy_data$nonrenewable.kwh = energy_data$total.kwh - energy_data$renewable.kwh
 
@@ -41,7 +44,7 @@ energyproduction = ggplot(largestkwh.stacked, aes(country, value, fill = Energy)
   coord_flip() +
   xlab("Countries") +
   ylab("Total Energy Output (kWh)") +
-  ggtitle("Countries that are the Largest Producers of Energy") +
+  ggtitle("Countries that are the Largest Producers of Energy, 2011") +
   theme_bw() +
   scale_fill_brewer(palette = 11)
 energyproduction
@@ -66,7 +69,57 @@ energypercentage = ggplot(largestrenewables, aes(country, renewable.percent)) +
   theme_bw() +
   xlab("Countries") +
   ylab("Renewable Energy Percentage") +
-  ggtitle("Countries w/ Largest Percentage of Renewables")
+  ggtitle("Countries w/ Largest Percentage of Renewables, 2011")
 energypercentage
 
-  
+################################ PREDICTIVE CAUSATION FOR THE US VS CHINA ###################################
+# Rename some of our columns
+colnames(usenergy) = c('years', 'renewable.kwh', 'total.kwh')
+usenergy$renewable.percent = usenergy$renewable.kwh/usenergy$total.kwh
+
+colnames(chnenergy) = c('years', 'renewable.kwh', 'total.kwh')
+chnenergy$renewable.percent = chnenergy$renewable.kwh/chnenergy$total.kwh
+
+################################ ENERGY AS A PERCENTAGE COMPARE ###################################
+# Create a Single Data Frame From the US and China energy datasets
+renewable = data.frame(usenergy$years, 
+                       usenergy$renewable.percent, 
+                       chnenergy$renewable.percent, 
+                       usenergy$total.kwh, 
+                       chnenergy$total.kwh)
+
+# Build the Linear Regression Models
+RenewablePercentageModel <- lm(usenergy.renewable.percent~chnenergy.renewable.percent, data=renewable)
+summary(RenewablePercentageModel)
+
+# Select and Melt the Data for Graphing
+renewablepercentage.stacked = renewable[,c(1,2,3)]
+colnames(renewablepercentage.stacked) = c('Years', 'US', 'China')
+renewablepercentage.melt = melt(renewablepercentage.stacked, id = c("Years"))
+
+# Graph the Data
+ggplot(renewablepercentage.melt,aes(x = Years, y = value, color = variable)) + 
+  geom_point(shape = 1) +
+  scale_colour_hue(l = 50) +
+  geom_smooth(method = loess) +
+  theme_bw() +
+  xlab("Years") +
+  ylab("Renewable Energy Percentage") +
+  ggtitle("Renewable Energy as a Percentage by Year, US and CHINA")
+
+################################ TOTAL ENERGY PRODUCTION COMPARE ###################################
+TotalEnergyModel <- lm(chnenergy.total.kwh~usenergy.total.kwh, data=renewable)
+summary(TotalEnergyModel)
+
+renewabletotal.stacked = renewable[,c(1,4,5)]
+colnames(renewabletotal.stacked) = c('Years', 'US', 'China')
+renewabletotal.melt = melt(renewabletotal.stacked, id = c("Years"))
+
+ggplot(renewabletotal.melt,aes(x = Years, y = value, color = variable)) + 
+  geom_point(shape = 1) +
+  scale_colour_hue(l = 50) +
+  geom_smooth(method = loess, se = FALSE) +
+  theme_bw() +
+  xlab("Years") +
+  ylab("Renewable Energy Production (kWh)") +
+  ggtitle("Renewable Energy in kWh by Year, US and CHINA")
---
+++ EnergyData.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 12 05:07:07 2014
+
+@author: johnjsyoo
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 11 20:51:52 2014
+
+@author: johnjsyoo
+"""
+
+import wbdata
+import datetime
+import pandas as pd
+import MySQLdb as myDB
+
+######################### GETTING ENERGY DATA FOR ALL COUNTRIES ##############################
+
+# Setting the date range for our data
+data_date = (datetime.datetime(2011, 1, 1))
+             
+# GDP (current US$)
+gdp = wbdata.get_data("NY.GDP.MKTP.CD", data_date=data_date, pandas = True)[44:]
+    
+# Data on Electricity production from renewable sources (kWh)
+renewableProd = wbdata.get_data("EG.ELC.RNEW.KH", data_date=data_date, pandas = True)[44:]
+electricityProd = wbdata.get_data("EG.ELC.PROD.KH", data_date=data_date, pandas = True)[44:]
+
+# Convert the time-series data into a Data Frame
+gdpDF = pandas.DataFrame(gdp)
+renewableProdDF = pandas.DataFrame(renewableProd)
+electricityProdDF = pandas.DataFrame(electricityProd)
+
+energyDF = gdpDF.join(renewableProdDF, lsuffix="GDP",rsuffix="Renewable_kWh")
+energyDF = energyDF.join(electricityProdDF)
+energyDF.rename(columns = {'value':'Electricity_kWh'}, inplace = True)
+
+# Dropping all NaN values and zero values
+energyDF = energyDF[energyDF.valueRenewable_kWh != 0]
+energyDF = energyDF.dropna()
+
+######################### GETTING PREDICTIVE DATA FOR US MODEL ##############################
+
+countryList = ["USA", "CHN"]
+
+renewableProdUS = []
+electricityProdUS = []
+
+renewableProdCHN = []
+electricityProdCHN = []
+
+# Function for grabbing energy data for US and China
+def stealingEnergy(countryname):
+    data_dateUSC = (datetime.datetime(1971, 1, 1), datetime.datetime(2011, 1, 1))
+    
+   # Data on Electricity production from renewable sources (kWh) for US
+    if countryname == "USA":    
+        renewableProdUS.extend(wbdata.get_data("EG.ELC.RNEW.KH", data_date=data_dateUSC, country="USA", pandas = True))
+        electricityProdUS.extend(wbdata.get_data("EG.ELC.PROD.KH", data_date=data_dateUSC, country="USA", pandas = True))
+    else:
+        # Data on Electricity production from renewable sources (kWh) for CHN
+        renewableProdCHN.extend(wbdata.get_data("EG.ELC.RNEW.KH", data_date=data_dateUSC, country="CHN", pandas = True))
+        electricityProdCHN.extend(wbdata.get_data("EG.ELC.PROD.KH", data_date=data_dateUSC, country="CHN", pandas = True))
+
+for country in countryList:
+    stealingEnergy(country)
+
+# Reversing the order of years so that it matches our dictionary values
+years = range(1971, 2012)
+yearsreversed = years[::-1]
+
+usenergy = pd.DataFrame({'years': yearsreversed, 'renewable': renewableProdUS, 'total': electricityProdUS})
+usenergy = usenergy.set_index('years')
+                         
+chnenergy = pd.DataFrame({'years': yearsreversed, 'renewable': renewableProdCHN, 'total': electricityProdCHN})
+chnenergy = chnenergy.set_index('years')
+
+######################### EXPORTING THE DATA AND CONNECTING TO MYSQL ##############################
+
+# Reading the data to CSV
+energyDF.to_csv('/users/johnjsyoo/Dropbox/IndividualAssignment/energy_data.csv')
+usenergy.to_csv('/users/johnjsyoo/Dropbox/IndividualAssignment/usenergy.csv')
+chnenergy.to_csv('/users/johnjsyoo/Dropbox/IndividualAssignment/chnenergy.csv')
+
+# World Energy Data
+dbConnect = myDB.connect(host='localhost',
+                         user='root',
+                         passwd='LAX',
+                         db='wbproject')
+
+energyDF.to_sql(con=dbConnect,
+                name='energydata',
+                if_exists='replace',
+                flavor='mysql')
+
+# US Energy Data         
+usenergy.to_sql(con=dbConnect,
+                name='usenergy',
+                if_exists='replace',
+                flavor='mysql')
+                
+# China Energy Data         
+chnenergy.to_sql(con=dbConnect,
+                 name='chnenergy',
+                 if_exists='replace',
+                 flavor='mysql')
+        
